6f1197963672bff79707bacae7168c14077beab6
ollie <lieshoff@gmx.de>
2025-07-07 20:39:23 +0200

    Removed file system port and moved ports to service interfaces package.

--- service/src/main/java/de/ollie/ucg/core/service/port/FileSystemPort.java
+++
@@ -1,7 +0,0 @@
-package de.ollie.ucg.core.service.port;
-
-import de.ollie.ucg.core.model.GeneratorConfiguration;
-
-public interface FileSystemPort {
-	void storeClassFile(GeneratorConfiguration generatorConfiguration, String classFileContent);
-}
